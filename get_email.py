a8d2af549a4ac05c3701f751a1c0ac085bf8aef4
Luca stefanescu <lucastu2013@gmail.com>
2023-04-25 22:54:38 +0000

    changes

--- get_email.py
+++ get_email.py
@@ -2,8 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-response = requests.get("https://intropic.io/")
+response = input("Enter the website url: ") 
 soup = BeautifulSoup(response.text, 'lxml')
+pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 ite = re.finditer("mailto", response.text)
 indices = [m.start(0) for m in ite]
+emails = []
+
+for i in indices:
+    emails.append(re.findall(pattern, response.text[i:i+40])[0])
+
+print(emails)
